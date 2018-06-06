4e5878cd4b9f4bc7030ff8220a99d12b3272cbc6
hyh <m15517208396@163.com>
2018-06-06 16:29:25 +0800

    提交

---
+++ LoginActivity.java
@@ -0,0 +1,33 @@
+package com.desirephoto.game.pixel.activity;
+
+/**
+ * Created by admin on 2018/2/2.
+ */
+
+public class LoginActivity extends BaseAppCompatActivity implements ClientStandard.OnLoginResultListener, RtResultCallbackListener {   
+
+    @OnClick({R.id.login_facebook, R.id.login_google})
+    public void OnClick(View view) {
+        switch (view.getId()) {
+            case R.id.login_facebook:
+                platformFlag = FACEBOOK;
+                loginClient = ClientManager.getInstance(this).getFacebookLoginClient();
+                loginClient.setOnLoginResultListener(this);
+                loginClient.showLoginWindow();
+                break;
+            case R.id.login_google:
+                platformFlag = GOOGLE;
+                int statusCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+                if (statusCode != 0) {
+                    Toast.makeText(this, "No GooglePlayServices Found!", Toast.LENGTH_LONG).show();
+                } else {
+                    loginClient = ClientManager.getInstance(this).getGooglePlusLoginClient();
+                    loginClient.setOnLoginResultListener(this);
+                    loginClient.showLoginWindow();
+                }
+                break;
+        }
+    }
+
+   
+}
